5d2c610fa70e0aca73cc2a4cf13f8763f67f2ca0
Sam Price <sam@typicalsamprice.com>
2024-03-23 10:41:09 -0500

    Initial Commit

--- start-document.sh
+++ start-document.sh
@@ -6,3 +6,8 @@ printf "%s\n" "'${DIR}' is the current directory, making document setup here."
 
 ln -s "$HOME/Git/latex-template/template.tex" ./template.tex
 cp "$HOME/Git/latex-template/document.tex" ./document.tex
+
+if [ -n "${1}" ]; then
+    mv ./document.tex "${1}".tex
+    printf "Renamed document.tex to %s.tex" "${1}"
+fi
