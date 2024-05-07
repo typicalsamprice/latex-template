8afc8fe7e9adaecde3aed140be4982a65012e02c
Sam Price <sam@typicalsamprice.com>
2024-05-07 00:32:36 -0500

    Template 2

--- start-document.sh
+++ start-document.sh
@@ -4,7 +4,7 @@ DIR="$(pwd)"
 
 printf "%s\n" "'${DIR}' is the current directory, making document setup here."
 
-ln -s "$HOME/Git/latex-template/template.tex" ./template.tex
+ln -s "$HOME/Git/latex-template/template-2.tex" ./template.tex
 
 if [ "${1}" = "-l" ]; then
     exit 0
