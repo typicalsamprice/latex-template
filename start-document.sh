87a5d657f6a7372283d9b74a2c443432a118e835
Sam Price <sam@typicalsamprice.com>
2024-06-29 23:56:07 -0500

    Rewrote template

--- start-document.sh
+++ start-document.sh
@@ -4,7 +4,9 @@ DIR="$(pwd)"
 
 printf "%s\n" "'${DIR}' is the current directory, making document setup here."
 
-ln -s "$HOME/Git/latex-template/template-2.tex" ./template.tex
+ln -s "$HOME/Git/latex-template/template.tex" ./template.tex
+ln -s "$HOME/Git/latex-template/theoremboxes.tex" ./theoremboxes.tex
+ln -s "$HOME/Git/latex-template/letterfonts.tex" ./letterfonts.tex
 
 if [ "${1}" = "-l" ]; then
     exit 0
@@ -15,4 +17,5 @@ cp "$HOME/Git/latex-template/document.tex" ./document.tex
 if [ -n "${1}" ]; then
     mv ./document.tex "${1}".tex
     printf "Renamed document.tex to %s.tex" "${1}"
+    sed -i "s/document\\.tex/${1}.tex" ./"${1}".tex
 fi
